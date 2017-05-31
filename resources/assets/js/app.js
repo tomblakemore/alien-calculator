95144be54aabfd64321aa1aa2498dce935c29173
Tom Blakemore <tom.blakemore@mac.com>
2017-05-31 02:11:32 +0100

    Adding a basic form

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -15,8 +15,11 @@ window.Vue = require('vue');
  * or customize the JavaScript scaffolding to fit your unique needs.
  */
 
-Vue.component('example', require('./components/Example.vue'));
-
 const app = new Vue({
-    el: '#app'
+    el: '#app',
+    data: {
+        operand1: '',
+        operand2: '',
+        operator: ''
+    }
 });
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,92 +4,45 @@
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-
-        <title>Laravel</title>
-
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
-
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Raleway', sans-serif;
-                font-weight: 100;
-                height: 100vh;
-                margin: 0;
-            }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 12px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+        <title>{{ config('app.name') }}</title>
+        <link rel="stylesheet" href="{{ asset('css/app.css') }}" />
     </head>
     <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @if (Auth::check())
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ url('/login') }}">Login</a>
-                        <a href="{{ url('/register') }}">Register</a>
-                    @endif
+        <div id="app">
+            <nav class="navbar navbar-inverse navbar-fixed-top">
+                <div class="container">
+                    <div class="navbar-header">
+                        <a class="navbar-brand" href="/">{{ config('app.name') }}</a>
+                    </div>
                 </div>
-            @endif
-
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
+            </nav>
+            <div class="container">
+                <h1>{{ config('app.name') }}</h1>
+                <p class="lead">Enter the two operands below and choose an operator.</p>
+                <form class="form-inline" method="post">
+                    {{ csrf_field() }}
+                    <div class="form-group">
+                        <label class="sr-only" for="value1">First operand</label>
+                        <input type="text" class="form-control" id="operand1" placeholder="First operand" v-model="operand1">
+                    </div>
+                    <div class="form-group">
+                        <label class="sr-only" for="operand2">Password</label>
+                        <input type="text" class="form-control" id="operand2" placeholder="Second operand" v-model="operand2">
+                    </div>
+                    <div class="form-group">
+                        <select class="form-control" v-model="operator">
+                            <option value="" selected>Operator &#8230;</option>
+                            <option value="alien">&#128125; Alien</option>
+                            <option value="skull">&#128128; Skull</option>
+                            <option value="ghost">&#128123; Ghost</option>
+                            <option value="scream">&#128561; Scream</option>
+                        </select>
+                    </div>
+                    <button type="submit" class="btn btn-default">Calculate</button>
+                </form>
             </div>
         </div>
+        <script src="{{ asset('js/app.js') }}"></script>
     </body>
 </html>
