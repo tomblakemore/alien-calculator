131368e425e5396610bfb1d9f2fd3a508ff1eb57
Tom Blakemore <tom.blakemore@mac.com>
2017-06-01 00:10:10 +0100

    Adding some more styles

--- resources/views/calculator.blade.php
+++ resources/views/calculator.blade.php
@@ -13,15 +13,23 @@
             <nav class="navbar navbar-inverse navbar-fixed-top">
                 <div class="container">
                     <div class="navbar-header">
+                        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+                            <span class="sr-only">Toggle navigation</span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                        </button>
                         <a class="navbar-brand" href="/">{{ config('app.name') }}</a>
                     </div>
-                    <ul class="nav navbar-nav navbar-right">
-                        <li>
-                            <a href="https://github.com/tomblakemore/alien-calculator">
-                                <img class="github-logo" src="{{ asset('images/github-logo.png') }}">
-                            </a>
-                        </li>
-                    </ul>
+                    <div id="navbar" class="collapse navbar-collapse">
+                        <ul class="nav navbar-nav navbar-right">
+                            <li>
+                                <a class="github-link" href="https://github.com/tomblakemore/alien-calculator">
+                                    <img src="{{ asset('images/github-logo.png') }}">
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
                 </div>
             </nav>
 
